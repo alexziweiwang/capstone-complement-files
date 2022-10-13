260020ad68c05b8e69f0bac87124a40218200b78
Alex(Ziwei) Wang <alexiswayne9912@gmail.com>
2022-10-13 04:16:35 -0700

    Update torgo_make_mfcc.sh

--- torgo_make_mfcc.sh
+++ torgo_make_mfcc.sh
@@ -10,7 +10,7 @@
 
 # Begin configuration section.
 nj=8
-cmd=!bash /content/torgo_asr/run.pl
+cmd=!perl /content/torgo_asr/run.pl
 mfcc_config=/content/torgo_asr/conf/mfcc.conf
 mfcc_config_dys=/content/torgo_asr/conf/mfcc_dysarthric.conf  # can of course also be conf/mfcc.conf
 compress=true
